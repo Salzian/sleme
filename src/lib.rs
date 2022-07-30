2c759e90495b290f34e4a667836aeb59f9966f5e
Salzian <salzian.dev@gmail.com>
2022-07-31 00:13:44 +0200

    Parse request form data

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-use worker::{console_log, Context, Date, Env, event, Request, Response, Result, Router};
+extern crate core;
 
+use slash_command_form_data::SlashCommandFormData;
+use worker::{
+    console_debug, console_log, event, Context, Date, Env, Request, Response, Result, Router,
+};
+
+mod slash_command_form_data;
 mod utils;
 
 fn log_request(req: &Request) {
@@ -13,12 +19,22 @@ fn log_request(req: &Request) {
 }
 
 #[event(fetch)]
-pub async fn main(request: Request, env: Env, _ctx: Context) -> Result<Response> {
+pub async fn main(request: Request, env: Env, _context: Context) -> Result<Response> {
     log_request(&request);
     utils::set_panic_hook();
 
     let router = Router::new();
-    router.post_async("/commands/meme", |_, _| async {
-        Response::ok("meme")
-    }).run(request, env).await
+
+    router
+        .post_async("/commands/meme", |mut request, _context| async move {
+            // Log request query parameters
+            let form_data = request.form_data().await.unwrap();
+            let parsed_form_data = SlashCommandFormData::from_form_data(form_data);
+
+            console_debug!("{:#?}", parsed_form_data);
+
+            Response::empty()
+        })
+        .run(request, env)
+        .await
 }
